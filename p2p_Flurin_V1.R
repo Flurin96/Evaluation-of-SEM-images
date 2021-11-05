31b705b2366c5ca0ac187d0ab73f19cdc93a385e
Flurin96 <73837045+Flurin96@users.noreply.github.com>
2021-11-05 15:26:23 +0100

    Update p2p_Flurin_V1.R

--- p2p_Flurin_V1.R
+++ p2p_Flurin_V1.R
@@ -7,15 +7,12 @@
 # +41 79 838 27 72
 # flurinmahtis@gmail.com
 
-
 p2p <- function() # end with ctrl-click or Esc
 { 
-  
-  n = 512
+    n = 512
   ans1 <- numeric()
   ans2 <- numeric()
-  
-  
+   
   #Set know distance from scale bar via text box
   message1 = "Set Length of the scale bar:"
   Dialog1.input = dlgInput(message1)
@@ -29,13 +26,10 @@ p2p <- function() # end with ctrl-click or Esc
     a <- locator(2, type = "o", pch = 3, col = "blue1")
   meassured_knownDistance <- sqrt(sum(sapply(a, diff)^2))
   TransformFactor = set_knownDistance / meassured_knownDistance # correction factor
-  
-  
+    
   # Measure the length bacteria cells
   message3 = "Click on the Star and End Point of the bacteria cells."
   Dialog3 = winDialog(type = c("okcancel"), message3)
-  
-  #### Hier Weitermachen. Loop unten muss selektiv beendet werden können sobald alle Zell-Längen vermessen wurden.
   
   while (n > 0) {
     # this call to locator places 2 points as crosses
@@ -45,13 +39,11 @@ p2p <- function() # end with ctrl-click or Esc
     
     ans1 <- c(ans1, sqrt(sum(sapply(p, diff)^2)))
     n <- n - 1
-    
-  }
+   }
     ans1 = ans1 * TransformFactor
   
   return(ans1) # return the vector of length
  
-  
   while (n > 0) {
     # this call to locator places 2 points as crosses
     # and connects them with a line for the wide of cells
@@ -63,6 +55,5 @@ p2p <- function() # end with ctrl-click or Esc
   }
   ans2 = ans2 * TransformFactor
   return(ans2) # return the vector of wide
-  
 }
 
